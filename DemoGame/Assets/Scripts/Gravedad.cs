07891fa4b6b8e3782ad55316dea11ec349482fc3
Angel <aramirez@voicetechsolutions.com.mx>
2019-04-23 20:38:02 -0500

    los Scripts de oaxa

---
+++ DemoGame/Assets/Scripts/Gravedad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Gravedad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Planeta.planeta.objetos.Add(GetComponent<Rigidbody>());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DemoGame/Assets/Scripts/Planeta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planeta : MonoBehaviour
+{
+    public static Planeta planeta;
+    public float fuerzaGravitacional;
+    public List<Rigidbody> objetos;
+    void Awake()
+    {
+        planeta = this;
+     }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (Rigidbody objeto in objetos)
+        {
+            Vector3 direccionGravedad = (objeto.transform.position - transform.position).normalized;
+            Vector3 direccionObjeto = objeto.transform.up;
+            objeto.AddForce(direccionGravedad * fuerzaGravitacional * Time.fixedDeltaTime);
+            Quaternion nRotation = Quaternion.FromToRotation(direccionObjeto, direccionGravedad) * objeto.transform.rotation;
+            objeto.transform.rotation = Quaternion.Slerp(objeto.transform.rotation, nRotation, 50 * Time.fixedDeltaTime);
+        }
+    }
+}
